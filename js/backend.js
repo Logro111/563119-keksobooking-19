39664f8c5faaf522d035ab8cb3203b4cc19b2e89
Logro111 <dekunin@gmail.com>
2020-03-09 00:27:02 +0300

    Исправляет недочеты

--- js/backend.js
+++ js/backend.js
@@ -3,29 +3,25 @@
 (function () {
   var TIMEOUT_IN_MS = 10000;
 
-  var loadURL = 'https://js.dump.academy/keksobooking/data';
+  var LOAD_URL = 'https://js.dump.academy/keksobooking/data';
   var statusCodes = {
     OK: 200
   };
 
-  var loadXhr = function (xhr, success, error) {
-    xhr.addEventListener('load', function () {
-      if (xhr.status === statusCodes.OK) {
-        success(xhr.response);
-      } else {
-        error('Статус ответа ' + xhr.status);
-      }
-    });
-  };
-
-  var load = function (onLoad, onError) {
+  var createHttpRequest = function (onLoad, onError, mainErrorMessage, url, requestType, data) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
-    loadXhr(xhr, onLoad, onError);
+    xhr.addEventListener('load', function () {
+      if (xhr.status === statusCodes.OK) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа ' + xhr.status);
+      }
+    });
 
     xhr.addEventListener('error', function () {
-      onError('Ошибка загрузки');
+      onError(mainErrorMessage);
     });
 
     xhr.addEventListener('timeout', function () {
@@ -34,11 +30,12 @@
 
     xhr.timeout = TIMEOUT_IN_MS;
 
-    xhr.open('GET', loadURL);
-    xhr.send();
+    xhr.open(requestType, url);
+    xhr.send(data);
   };
 
   window.backend = {
-    load: load
+    loadURL: LOAD_URL,
+    createHttpRequest: createHttpRequest
   };
 })();
