3e707e58af911db664d85dbf8d9cf62e9c1cab1a
Logro111 <dekunin@gmail.com>
2020-03-15 18:00:05 +0300

    Добавляет логику отрисовки и открытия карточки объявления, дополняет валидацию формы

--- js/form.js
+++ js/form.js
@@ -7,8 +7,19 @@
   var form = document.querySelector('.ad-form');
   var roomsField = form.querySelector('[name="rooms"]');
   var capacityField = form.querySelector('[name="capacity"]');
+  var houseTypeField = form.querySelector('[name="type"]');
+  var priceField = form.querySelector('[name="price"]');
+  var checkInField = form.querySelector('[name="timein"]');
+  var checkOutField = form.querySelector('[name="timeout"]');
 
-  var onFormChangeValidation = function () {
+  var houseTypeToMinPice = {
+    bungalo: 0,
+    flat: 1000,
+    house: 5000,
+    palace: 10000
+  };
+
+  var capacityValidation = function () {
     var capacityFieldValue = Number(capacityField.value);
     var roomsFieldValue = Number(roomsField.value);
     if (capacityFieldValue > roomsFieldValue) {
@@ -22,7 +33,33 @@
     }
   };
 
-  form.addEventListener('change', function () {
-    onFormChangeValidation();
+  var houseTypeValidation = function () {
+    var price = houseTypeToMinPice[houseTypeField.value];
+    priceField.setAttribute('placeholder', price);
+    priceField.setAttribute('min', price);
+    if (priceField.validity.rangeUnderflow) {
+      priceField.setCustomValidity('Минимальная цена при заданном типе жилья ( ' + window.card.houseTypeValuesMap[houseTypeField.value].toLowerCase() + ') - ' + price);
+    } else if (priceField.validity.rangeOverflow) {
+      priceField.setCustomValidity('Максимальная цена' + priceField.getAttribute('max'));
+    }
+  };
+
+  var timeValidation = function (evt) {
+    if (evt.target === checkInField) {
+      checkOutField.value = checkInField.value;
+    }
+    if (evt.target === checkOutField) {
+      checkInField.value = checkOutField.value;
+    }
+  };
+
+  var onFormChangeValidation = function (evt) {
+    capacityValidation();
+    houseTypeValidation();
+    timeValidation(evt);
+  };
+
+  form.addEventListener('change', function (evt) {
+    onFormChangeValidation(evt);
   });
 })();
--- js/map.js
+++ js/map.js
@@ -3,6 +3,7 @@
 (function () {
 
   var mainMapPin = document.querySelector('.map__pin--main');
+  var pinsContainer = document.querySelector('.map__pins');
 
   mainMapPin.addEventListener('mousedown', function (evt) {
     window.util.isMainMouseButtonEvent(evt, window.main.activate);
@@ -13,4 +14,31 @@
     window.util.isEnterEvent(evt, window.main.activate);
   },
   {once: true});
+
+  var onCardEscPress = function (evt) {
+    window.util.isEscEvent(evt, closeCard);
+  };
+
+  var closeCard = function () {
+    document.querySelector('.map__card').remove();
+    window.card.closeButton.removeEventListener('click', closeCard);
+    document.removeEventListener('keydown', onCardEscPress);
+  };
+
+  var openCard = function (evt) {
+    var pinData;
+    if (evt.target.getAttribute('data-pin-index')) {
+      pinData = window.pin.objectToPinMap[evt.target.dataset.pinIndex];
+      window.card.render(pinData);
+    } else if (evt.target.parentNode.getAttribute('data-pin-index')) {
+      pinData = window.pin.objectToPinMap[evt.target.parentNode.dataset.pinIndex];
+      window.card.render(pinData);
+    }
+    if (window.card.closeButton) {
+      window.card.closeButton.addEventListener('click', closeCard);
+      document.addEventListener('keydown', onCardEscPress);
+    }
+  };
+
+  pinsContainer.addEventListener('click', openCard);
 })();
